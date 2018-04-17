477531be4af2aed73c041f241fefbc7475fc9fb7
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-04-17 20:51:36 +0200

    tmp disable enable ISIG

--- src/terminal/terminal.c
+++ src/terminal/terminal.c
@@ -34,7 +34,8 @@ bool cs_term_enable_raw_mode() {
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  // raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN);
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)

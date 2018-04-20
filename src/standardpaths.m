7335abadc406a097ed894e2e8e0a9fe09b6439e6
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-04-21 01:27:37 +0200

    fixes

--- src/standardpaths.m
+++ src/standardpaths.m
@@ -26,7 +26,22 @@ char * osx_get_path(char *buffer, size_t len) {
     return buffer;
 }
 
-char *get_path_osx(cs_stdpath_dir dir, cs_stdpath_domain domain) {
+static NSSearchPathDirectory cs_stdpath_dir_to_nssearchpath(cs_stdpath_dir dir) {
+    switch (dir) {
+    case cs_stdpath_data_dir:return NSApplicationSupportDirectory;
+    case cs_stdpath_cache_dir: return NSCachesDirectory;
+    }
+    return 0;
+}
+
+static NSSearchPathDomainMask cs_stdpath_domain_to_nssearchpath(cs_stdpath_domain domain) {
+    switch (domain) {
+    case cs_stdpath_user_domain: return NSUserDomainMask;
+    case cs_stdpath_local_domain: return NSLocalDomainMask;
+    }
+}
+
+char *get_path_osx(char *buffer, size_t len, cs_stdpath_dir dir, cs_stdpath_domain domain) {
     NSArray<NSURL*> *urls = [
         [NSFileManager defaultManager] 
         URLsForDirectory:NSApplicationSupportDirectory 
@@ -35,6 +50,33 @@ char *get_path_osx(cs_stdpath_dir dir, cs_stdpath_domain domain) {
 
     if ([urls count] == 0) return NULL;
 
+    NSSearchPathDirectory d = cs_stdpath_dir_to_nssearchpath(dir);
+
+    NSString *path = nil;
+
+    if (d == 0) {
+        path = [NSHomeDirectory() stringByAppendingPathComponent:@"/Library/Preferences"];
+    } else {
+        NSArray<NSURL*> *urls = [
+            [NSFileManager defaultManager] 
+            URLsForDirectory:NSApplicationSupportDirectory 
+            inDomains:NSUserDomainMask
+        ];
+
+        if ([urls count] == 0) return NULL;
+
+        path = [urls.firstObject absoluteString];
+    }
+
+    size_t slen = [path length];
+
+    if (slen > len) return NULL;
+
+    if (buffer == NULL)
+        buffer = malloc(sizeof(char)*slen);
     
+    memcpy(buffer, path.UTF8String, slen);
+
+    return buffer;
 
 }
